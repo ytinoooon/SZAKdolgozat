026fb6a59e517da71e079c0367962148870ff4f0
ytinoooon <79020731+ytinoooon@users.noreply.github.com>
2024-07-18 17:19:58 +0300

    Update sketch.js
    replikalodas

--- 1step/sketch.js
+++ 1step/sketch.js
@@ -1,15 +1,22 @@
 let entities = [];
-let maxEntities = 20;
+let orangealive = 0;
 let births = 0;
 let deaths = 0;
-let birth_rate = 7;
-let death_rate = 3;
+let birth_rate = 100;
+function sign(x) {
+    return x === 0 ? 0 : x > 0 ? 1 : -1;
+}
 function setup() {
     createCanvas(1470,690);
     frameRate(5);
-    for(let i = 0; i < maxEntities; i++){
-        entities[i] = new entity(cos(i)*600+735,sin(i)*300+345);
-
+    for(let i = 0; i<5;i++){
+        let k = random(i)*2;
+        let x = 400 * pow(abs(cos(k)), 2/10) * sign(cos(k))+500;
+        let y = 220 * pow(abs(sin(k)), 2/10) * sign(sin(k))+250;
+        console.log(x,y);
+        entities[i] = new entity(x,y,[5,5,5,5,5,5],"lightgreen");
+        entities[i].alive = true;
+        births += 1; 
     }
 }
 
@@ -18,34 +25,35 @@ function draw() {
     text("Alive: "+String(births-deaths), 30, 40); 
     text("Births: "+String(births), 30, 60); 
     text("Deaths: "+String(deaths), 30, 80); 
-    text("Birth rate: "+String(birth_rate), 30, 100);
-    text("Death rate: "+String(death_rate), 30, 120);
+    // text("OrangeAlive: "+String(orangealive), 30, 100); 
 
-    for(let i = 0; i < maxEntities; i++){
-        if(entities[i].alive) {
-            entities[i].show();
-            entities[i].timealive += 1;
-            entities[i].radius++;
-              if(entities[i].timealive%5==0){
-                entities[i].acolor -= 10;
-                if(entities[i].acolor<0){
-                    entities[i].acolor=0;
-                }
-              }
-        }
-        if(random(entities[i].barr) && !entities[i].alive) { 
-            entities[i].alive = true;
-            births += 1;
-        } 
-        if(random(entities[i].darr) && entities[i].alive) {
-            entities[i].alive = false;
+
+    for(let i = 0; i < entities.length; i++){
+        entities[i].show();
+        entities[i].age();
+        if(entities[i].timealive > random(15) && entities[i].alive){
+            entities[i].die();
             deaths += 1;
-            entities[i].timealive=0;
-            entities[i].acolor=255;
-            entities[i].radius=8;
+            if(entities[i].color == "orange"){
+                orangealive =- 1;
+            }
         }
-        
+        if(entities[i].alive && random(arr(entities[i].raplicate))){
+            entities.push(new entity(random(1460),random(680),entities[i].chrom,"orange"));
+            entities[entities.length-1].alive = true;
+            births += 1;
+            orangealive += 1; 
+        }
     }
+    if(random(arr(birth_rate))){
+        entities.push(new entity(random(1460),random(680),[5,5,5,5,5,5],"lightgreen"));
+        entities[entities.length-1].alive = true;
+        births += 1;
 
-}
-
+    }
+    for(let i = 0; i<entities.length;i++){
+        if(!entities[i].alive){
+            let spliced = entities.splice(i, 1);
+        }    
+    }
+ }
