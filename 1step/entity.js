c6202c85ccd69ac34362eb6e88c14562204da1f9
ytinoooon <79020731+ytinoooon@users.noreply.github.com>
2024-07-24 14:42:28 +0300

    Update entity.js

--- 1step/entity.js
+++ 1step/entity.js
@@ -9,17 +9,22 @@ function arr(num) {
     }
     return a;
 }
+// chrom tomb [[red,blue,green],radius,v_radius,speed,longevaty,replicate]
+                  
 
 class entity {
-    constructor(x,y,chrom,color) {
+    constructor(x,y,chrom) {
         this.x = x;
         this.y = y;
-        this.raplicate = 8;
-        this.radius = 10;
         this.alive = false;
-        this.timealive = 0;
+        this.time_alive = 0; 
         this.chrom = chrom;
-        this.color = color;
+        this.color = this.chrom[0]; //express color as an array and turn it into hex 
+        this.radius = this.chrom[1];
+        this.v_radius = this.chrom[2];
+        this.speed = this.chrom[3];
+        this.longevaty = this.chrom[4];
+        this.replicate = this.chrom[5];
     }
     show() {
         if(this.alive){
@@ -27,6 +32,9 @@ class entity {
             fill(this.color);
             circle(this.x,this.y,this.radius);
         }
+    }
+    fed(){ 
+        timealive -= 1;
     }
     age(){
         if(this.alive){
