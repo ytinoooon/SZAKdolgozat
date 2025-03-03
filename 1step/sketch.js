7fb408eb7dc91c9538912f4e8c188ab2c7e5322b
ytinoooon <79020731+ytinoooon@users.noreply.github.com>
2025-03-03 20:22:13 +0200

    Update sketch.js

--- 1step/sketch.js
+++ 1step/sketch.js
@@ -1,4 +1,5 @@
 let entities = [];
+let foods = [];
 let births = 0;
 let deaths = 0;
 
@@ -8,22 +9,27 @@ function setup() {
     for (let i = 0; i < 10; i++) {
         // chrom tomb [[red, blue, green], radius, v_radius, speed, longevaty, replicate]
         entities.push(new entity(random(1450), random(670), [[255, 150, 0], 30, 15, 3, 20, 5]));
+        foods.push(new food(random(1450),random(670)))
         births += 1;
     }
 }
 
 function draw() {
     background(100);
+    frameRate(5)
     console.log(entities)
     text("Alive: " + String(births - deaths), 30, 40);
     text("Births: " + String(births), 30, 60);
     text("Deaths: " + String(deaths), 30, 80);
-
+    for(let i = 0; i < 10;i++){
+        foods[i].show();
+    }
     // Loop through entities
     for (let i = entities.length - 1; i >= 0; i--) {
         let actual = entities[i];
         actual.show();
         actual.age();
+        actual.move(random(10),random(10));
 
         // Die if timealive exceeds longevaty
         if (actual.timealive > actual.longevaty && actual.alive) {
@@ -45,4 +51,5 @@ function draw() {
         }
     }
 }
+
 
