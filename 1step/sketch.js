1db893666c57a483a7f46c2108d01cc795100fa2
Tamas Peter-Brezovsky <tomcsi.brezo@gmail.com>
2025-03-05 07:49:57 +0200

    Update sketch.js

--- 1step/sketch.js
+++ 1step/sketch.js
@@ -1,5 +1,4 @@
 let entities = [];
-let foods = [];
 let births = 0;
 let deaths = 0;
 
@@ -8,28 +7,24 @@ function setup() {
     frameRate(5);
     for (let i = 0; i < 10; i++) {
         // chrom tomb [[red, blue, green], radius, v_radius, speed, longevaty, replicate]
-        entities.push(new entity(random(1450), random(670), [[255, 150, 0], 30, 15, 3, 20, 5]));
-        foods.push(new food(random(1450),random(670)))
+        entities.push(new entity(random(1450), random(670), [[random(255), random(255), random(255)], 30, 15, 3, 15, 7]));
         births += 1;
     }
 }
 
 function draw() {
     background(100);
-    frameRate(5)
     console.log(entities)
     text("Alive: " + String(births - deaths), 30, 40);
     text("Births: " + String(births), 30, 60);
     text("Deaths: " + String(deaths), 30, 80);
-    for(let i = 0; i < 10;i++){
-        foods[i].show();
-    }
+    
+
     // Loop through entities
     for (let i = entities.length - 1; i >= 0; i--) {
         let actual = entities[i];
         actual.show();
         actual.age();
-        actual.move(random(10),random(10));
 
         // Die if timealive exceeds longevaty
         if (actual.timealive > actual.longevaty && actual.alive) {
@@ -39,7 +34,7 @@ function draw() {
 
         // Replicate with a chance based on the replicate rate
         if (actual.alive && random(100) <= actual.replicate) {
-            let offspring = new entity(random(1460), random(680), actual.chrom);
+            let offspring = new entity(actual.x+(Math.round(Math.random())-0.5)*40, actual.y+(Math.round(Math.random())-0.5)*40, actual.chrom);
             mutation(offspring.chrom);
             entities.push(offspring);
             births += 1;
