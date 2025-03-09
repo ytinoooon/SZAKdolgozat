58a3bcacba8a46c175861c0575f3f72e229155b1
ytinoooon <79020731+ytinoooon@users.noreply.github.com>
2025-03-09 22:13:58 +0200

    Mozgas valahonnan valahova

--- 1step/entity.js
+++ 1step/entity.js
@@ -17,7 +17,7 @@ class entity {
         this.y = y;
         this.alive = true;
         this.timealive = 0; 
-        this.chrom = JSON.parse(JSON.stringify(chrom));
+        this.chrom = chrom;
         this.color = this.chrom[0]; //express color as an array and turn it into hex 
         this.radius = this.chrom[1];
         this.v_radius = this.chrom[2];
@@ -37,6 +37,19 @@ class entity {
             this.timealive+= 1;
         }
     }
+    move(target_x,target_y) {
+        let dx = target_x - this.x;
+        let dy = target_y - this.y;
+        let dist = Math.sqrt(dx*dx+dy*dy)
+        if(dist < this.speed){
+            this.x = target_x;
+            this.y = target_y;
+            return;
+        }
+        const angle = Math.atan2(dy,dx);
+        this.x += Math.cos(angle) * this.speed;
+        this.y += Math.sin(angle) * this.speed;
+    }
     die(){
         if(this.alive){
             this.alive = false;
@@ -49,11 +62,14 @@ class food {
         this.x = x;
         this.y = y;
         this.r = 6;
+        this.capacity = 100;
     }
+
     show() {
         noStroke();
         fill(color(300,170,137))
         circle(this.x,this.y,this.r);
     }
 }
+
 
