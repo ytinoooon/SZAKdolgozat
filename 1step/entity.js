9eb4c4b9a48f849b058752dae812c12d5a8579d3
ytinoooon <79020731+ytinoooon@users.noreply.github.com>
2025-04-08 08:18:31 +0300

    Update entity.js

--- 1step/entity.js
+++ 1step/entity.js
@@ -1,117 +1,87 @@
-let entities = [];
-let foods = [];
-let births = 0;
-let deaths = 0;
+function mutation(arr) {
+    arr[0][0] = constrain(arr[0][0] + random(-10, 10), 0, 255);
+    arr[0][1] = constrain(arr[0][1] + random(-10, 10), 0, 255);
+    arr[0][2] = constrain(arr[0][2] + random(-10, 10), 0, 255);
+    arr[1] = constrain(arr[1] + random(-5, 5), 10, 60);
+    arr[2] = constrain(arr[2] + random(-10, 10), 20, 150);
+    arr[3] = constrain(arr[3] + random(-1, 1), 1, 10);
+    arr[4] = constrain(arr[4] + random(-10, 10), 20, 300);
+    // arr[5] = constrain(arr[5] + random(-0.5, 0.5), 0, 5);
+}
 
-function setup() {
-    createCanvas(1470, 690);
-    frameRate(30); // smoother frame rate
-    for (let i = 0; i < 15; i++) {
-        entities.push(new entity(random(1450), random(670), [[random(255), random(255), random(255)], 30, 100, 5, 100, 1]));
-        births++;
+class entity {
+    constructor(x, y, chrom) {
+        this.x = x;
+        this.y = y;
+        this.alive = true;
+        this.timealive = 0;
+        this.chrom = chrom;
+        this.color = this.chrom[0];
+        this.radius = this.chrom[1];
+        this.v_radius = this.chrom[2];
+        this.speed = this.chrom[3];
+        this.longevaty = this.chrom[4];
+        this.replicate = this.chrom[5];
+        this.hungry = 0;
     }
 
-    for (let i = 0; i < 30; i++) {
-        foods.push(new food(random(width), random(height)));
+    show() {
+        if (this.alive) {
+            fill(this.color[0], this.color[1], this.color[2]);
+            noStroke();
+            circle(this.x, this.y, this.radius * 2);
+        }
+    }
+
+    age() {
+        if (this.alive) this.timealive++;
+    }
+
+    move(tx, ty) {
+        let dx = tx - this.x;
+        let dy = ty - this.y;
+        let dist = Math.sqrt(dx * dx + dy * dy);
+        if (dist < this.speed) {
+            this.x = tx;
+            this.y = ty;
+            return;
+        }
+        const angle = Math.atan2(dy, dx);
+        this.x += Math.cos(angle) * this.speed;
+        this.y += Math.sin(angle) * this.speed;
+        this.x = constrain(this.x, 0, width);
+        this.y = constrain(this.y, 0, height);
+    }
+
+    in_vision(x, y) {
+        let dx = x - this.x;
+        let dy = y - this.y;
+        return dx * dx + dy * dy <= (this.v_radius + this.radius) ** 2;
+    }
+
+    eating(x, y) {
+        let dx = x - this.x;
+        let dy = y - this.y;
+        return dx * dx + dy * dy <= this.radius ** 2;
+    }
+
+    die() {
+        this.alive = false;
     }
 }
 
-function draw() {
-    background(100);
-    fill(255);
-    text("Alive: " + (births - deaths), 30, 40);
-    text("Births: " + births, 30, 60);
-    text("Deaths: " + deaths, 30, 80);
-
-    // Show foods
-    for (let i = 0; i < foods.length; i++) {
-        foods[i].show();
+class food {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.r = 6;
+        this.color = "blue";
+        this.capacity = 100;
     }
 
-    // Handle entities
-    for (let i = entities.length - 1; i >= 0; i--) {
-        let actual = entities[i];
-        if (!actual.alive) {
-            entities.splice(i, 1);
-            continue;
-        }
-
-        actual.age();
-        actual.show();
-
-        // Detect nearby food
-        let foodInSight = [];
-        for (let j = 0; j < foods.length; j++) {
-            if (actual.in_vision(foods[j].x, foods[j].y)) {
-                foodInSight.push(foods[j]);
-                foods[j].color = color(225, 225, 225);
-            }
-        }
-
-        // Movement decision
-        if (foodInSight.length > 0) {
-            actual.move(foodInSight[0].x, foodInSight[0].y);
-        } else if (actual.hungry > 0 && foods.length > 0) {
-            let closest = null;
-            let minDist = Infinity;
-            for (let j = 0; j < foods.length; j++) {
-                let dx = foods[j].x - actual.x;
-                let dy = foods[j].y - actual.y;
-                let dist = dx * dx + dy * dy;
-                if (dist < minDist) {
-                    minDist = dist;
-                    closest = foods[j];
-                }
-            }
-            if (closest) {
-                actual.move(closest.x, closest.y);
-            }
-        } else {
-            actual.move(actual.x + random(-20, 20), actual.y + random(-20, 20));
-        }
-
-        // Eating
-        for (let j = 0; j < foods.length; j++) {
-            if (actual.eating(foods[j].x, foods[j].y) && foods[j].capacity > 0) {
-                actual.hungry = max(0, actual.hungry - 20);
-                foods[j].capacity -= 20;
-                if (foods[j].capacity <= 0) {
-                    foods.splice(j, 1);
-                }
-                actual.longevaty += 5;
-                break;
-            }
-        }
-
-        if(actual.hungry >= 10){
-            actual.longevaty -= 1
-        }
-
-        // Die if old
-        if (actual.timealive > actual.longevaty) {
-            actual.die();
-            deaths++;
-            continue;
-        }
-
-        // Replication
-        if (actual.alive && random(100) < actual.replicate) {
-            let clone = new entity(
-                actual.x + random(-40, 40),
-                actual.y + random(-40, 40),
-                structuredClone(actual.chrom)
-            );
-            mutation(clone.chrom);
-            entities.push(clone);
-            births++;
-        }
-
-        // Add hunger increase over time
-        actual.hungry += 0.5;
-    }
-
-    // Grow more food slowly
-    if (frameCount % 10 === 0 && foods.length < 100) {
-        foods.push(new food(random(width), random(height)));
+    show() {
+        fill(this.color);
+        noStroke();
+        circle(this.x, this.y, this.r);
     }
 }
