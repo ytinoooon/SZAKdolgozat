d2eef2625b0c3a0cc7e475478034c863da8fc23f
ytinoooon <79020731+ytinoooon@users.noreply.github.com>
2025-04-08 08:17:45 +0300

    Update entity.js

--- 1step/entity.js
+++ 1step/entity.js
@@ -1,64 +1,117 @@
-function arr(num) {
-    var a = [];
-    for(let i = 0;i < 100;i++) {
-        if(i < num) {
-            a[i] = true;
-        } else { 
-            a[i] = false;
-        }
-    }
-    return a;
-}
-// chrom tomb [[red,blue,green],radius,v_radius,speed,longevaty,replicate]
-                  
+let entities = [];
+let foods = [];
+let births = 0;
+let deaths = 0;
 
-class entity {
-    constructor(x,y,chrom) {
-        this.x = x;
-        this.y = y;
-        this.alive = false;
-        this.time_alive = 0; 
-        this.chrom = chrom;
-        this.color = this.chrom[0]; //express color as an array and turn it into hex 
-        this.radius = this.chrom[1];
-        this.v_radius = this.chrom[2];
-        this.speed = this.chrom[3];
-        this.longevaty = this.chrom[4];
-        this.replicate = this.chrom[5];
+function setup() {
+    createCanvas(1470, 690);
+    frameRate(30); // smoother frame rate
+    for (let i = 0; i < 15; i++) {
+        entities.push(new entity(random(1450), random(670), [[random(255), random(255), random(255)], 30, 100, 5, 100, 1]));
+        births++;
     }
-    show() {
-        if(this.alive){
-            noStroke();
-            fill(this.color);
-            circle(this.x,this.y,this.radius);
-        }
+
+    for (let i = 0; i < 30; i++) {
+        foods.push(new food(random(width), random(height)));
     }
-    fed(){ 
-        timealive -= 1;
-    }
-    age(){
-        if(this.alive){
-            this.timealive += 1;
-            this.radius += 0.5;
-        }
-    }
-    die(){
-        if(this.alive){
-            this.alive = false;
-        }
-    }
-    
 }
 
-// class food {
-//     constructor(x,y) {
-//         this.x = x;
-//         this.y = y;
-//         this.r = 6;
-//     }
-//     show() {
-//         noStroke();
-//         fill(color(300,170,137))
-//         circle(this.x,this.y,this.r);
-//     }
-// }
+function draw() {
+    background(100);
+    fill(255);
+    text("Alive: " + (births - deaths), 30, 40);
+    text("Births: " + births, 30, 60);
+    text("Deaths: " + deaths, 30, 80);
+
+    // Show foods
+    for (let i = 0; i < foods.length; i++) {
+        foods[i].show();
+    }
+
+    // Handle entities
+    for (let i = entities.length - 1; i >= 0; i--) {
+        let actual = entities[i];
+        if (!actual.alive) {
+            entities.splice(i, 1);
+            continue;
+        }
+
+        actual.age();
+        actual.show();
+
+        // Detect nearby food
+        let foodInSight = [];
+        for (let j = 0; j < foods.length; j++) {
+            if (actual.in_vision(foods[j].x, foods[j].y)) {
+                foodInSight.push(foods[j]);
+                foods[j].color = color(225, 225, 225);
+            }
+        }
+
+        // Movement decision
+        if (foodInSight.length > 0) {
+            actual.move(foodInSight[0].x, foodInSight[0].y);
+        } else if (actual.hungry > 0 && foods.length > 0) {
+            let closest = null;
+            let minDist = Infinity;
+            for (let j = 0; j < foods.length; j++) {
+                let dx = foods[j].x - actual.x;
+                let dy = foods[j].y - actual.y;
+                let dist = dx * dx + dy * dy;
+                if (dist < minDist) {
+                    minDist = dist;
+                    closest = foods[j];
+                }
+            }
+            if (closest) {
+                actual.move(closest.x, closest.y);
+            }
+        } else {
+            actual.move(actual.x + random(-20, 20), actual.y + random(-20, 20));
+        }
+
+        // Eating
+        for (let j = 0; j < foods.length; j++) {
+            if (actual.eating(foods[j].x, foods[j].y) && foods[j].capacity > 0) {
+                actual.hungry = max(0, actual.hungry - 20);
+                foods[j].capacity -= 20;
+                if (foods[j].capacity <= 0) {
+                    foods.splice(j, 1);
+                }
+                actual.longevaty += 5;
+                break;
+            }
+        }
+
+        if(actual.hungry >= 10){
+            actual.longevaty -= 1
+        }
+
+        // Die if old
+        if (actual.timealive > actual.longevaty) {
+            actual.die();
+            deaths++;
+            continue;
+        }
+
+        // Replication
+        if (actual.alive && random(100) < actual.replicate) {
+            let clone = new entity(
+                actual.x + random(-40, 40),
+                actual.y + random(-40, 40),
+                structuredClone(actual.chrom)
+            );
+            mutation(clone.chrom);
+            entities.push(clone);
+            births++;
+        }
+
+        // Add hunger increase over time
+        actual.hungry += 0.5;
+    }
+
+    // Grow more food slowly
+    if (frameCount % 10 === 0 && foods.length < 100) {
+        foods.push(new food(random(width), random(height)));
+    }
+}
